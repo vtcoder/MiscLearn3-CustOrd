a8fa9ee408ad18481f4623219a6fd9a3f7ce7393
John Doe <jdoe@local>
2016-08-03 16:31:36 -0400

    Inital checkin of mvc items, before adding packages.

---
+++ MiscLearn3-CustOrder/src/MiscLearn3-CustOrder/Models/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiscLearn3_CustOrder.Models
+{
+    public class HomeViewModel
+    {
+        public string HomeTitle { get; set; }
+    }
+}
