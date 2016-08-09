1f58c78de1b151725504ffe0edf90b8c8c89fa4b
John Doe <jdoe@local>
2016-08-09 14:00:29 -0400

    Added customer form field view component.

---
+++ MiscLearn3-CustOrder/src/MiscLearn3-CustOrder/Components/Customer/CustomerFormFieldsViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiscLearn3_CustOrder.Components.Customer
+{
+    public class CustomerFormFieldsViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
--- MiscLearn3-CustOrder/src/MiscLearn3-CustOrder/Models/CustomerViewModel.cs
+++ MiscLearn3-CustOrder/src/MiscLearn3-CustOrder/Models/CustomerViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,9 @@ namespace MiscLearn3_CustOrder.Models
 {
     public class CustomerViewModel
     {
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
     }
 }
