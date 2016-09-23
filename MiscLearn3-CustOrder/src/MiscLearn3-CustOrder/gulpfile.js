99670cc8afa5faae8113eadc6e1b2438bb502342
John Doe <jdoe@local>
2016-09-23 17:27:59 -0400

    Added gulp task to copy bower component output to wwwroot lib directory.

--- MiscLearn3-CustOrder/src/MiscLearn3-CustOrder/gulpfile.js
+++ MiscLearn3-CustOrder/src/MiscLearn3-CustOrder/gulpfile.js
@@ -8,3 +8,28 @@ var gulp = require('gulp');
 gulp.task('default', function () {
     // place code for your default task here
 });
+
+gulp.task('_output-bower-bootstrap', function () {
+    console.log('*** Copying bootstrap output files from bower components...');
+    var filesStream = gulp.src(
+        [
+            './bower_components/lib/bootstrap/dist/**/*.*',     /*Include all distribution files*/
+            '!./bower_components/lib/bootstrap/dist/**/npm.js'  /*Exclude NPM script file*/
+        ]);
+    filesStream.pipe(gulp.dest('./wwwroot/lib/bootstrap/'));
+    return filesStream;
+});
+
+gulp.task('_output-bower-jquery', function () {
+    console.log('*** Copying jquery output files from bower components...');
+    var filesStream = gulp.src(
+        [
+            './bower_components/lib/jquery/dist/**/*.*',     /*Include all distribution files*/
+            '!./bower_components/lib/jquery/dist/**/npm.js'  /*Exclude NPM script file*/
+        ]);
+    filesStream.pipe(gulp.dest('./wwwroot/lib/jquery/'));
+    return filesStream;
+});
+
+gulp.task('output-bower-distribution-content', ['_output-bower-bootstrap', '_output-bower-jquery'], function () {
+});
