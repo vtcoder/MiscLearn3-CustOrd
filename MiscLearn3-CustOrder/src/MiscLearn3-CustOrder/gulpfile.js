c9c55a73f1f6f2b11c596f73d2ed4162cad34b2c
John Doe <jdoe@local>
2016-09-28 16:50:10 -0400

    Added todo list, and bound gulp distribution task to vs.net build.

--- MiscLearn3-CustOrder/src/MiscLearn3-CustOrder/gulpfile.js
+++ MiscLearn3-CustOrder/src/MiscLearn3-CustOrder/gulpfile.js
@@ -1,4 +1,5 @@
-﻿/*
+/// <binding AfterBuild='output-bower-distribution-content' />
+/*
 This file in the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 */
